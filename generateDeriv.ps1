520076635cb922c6e6e0ef9c5ad17c475c8c686d
tallmann <tallmann@ucmail.uc.edu>
2015-07-11 17:40:22 -0400

    Clean up, more efficient thumbs

--- generateDeriv.ps1
+++ generateDeriv.ps1
@@ -1,30 +1,29 @@
 # UC Libraries, DigitzeUC Spokes
-# Generate derivatives for DRC using PowerShell and ImageMagick Portable
-# Nathan Tallman, July 2015
+# Generate derivatives for DRC
 
 Get-ChildItem "." -filter *.tif | `
 	Foreach {
 		$image = New-Object -ComObject Wia.ImageFile            
 		$image.LoadFile($_.FullName)            
 
-		If ($image.Width -gt 5000 -lt 20000 -Or $image.Height -gt 5000 -lt 20000)
+		If ($image.Width -gt 5000 -lt 15000 -Or $image.Height -gt 5000 -lt 15000)
 			{
-				.\ImageMagick\convert.exe $_.FullName -resample 300 -resize "5000x5000>" ($_.BaseName + ".jpg") 
+				.\ImageMagick\convert.exe -resample 300 -resize "5000x5000>" $_.Name ($_.BaseName + ".jpg") 
 				.\ImageMagick\convert.exe $_.Name ($_.BaseName + ".jp2") 
-				.\ImageMagick\convert.exe $_.Name -resample 96 -resize "100x100>" ($_.BaseName + ".jpg.jpg") 
+				.\ImageMagick\convert.exe -thumbnail 100x100 $_.Name ($_.BaseName + ".jpg.jpg") 
 			}
 
-		ElseIf ($image.Width -gt 20000 -Or $image.Height -gt 20000)
+		ElseIf ($image.Width -ge 15000 -Or $image.Height -ge 15000)
 			{
-				.\ImageMagick\convert.exe $_.FullName -resample 300 -resize "5000x5000>" ($_.BaseName + ".jpg") 
-				.\ImageMagick\convert.exe -resize "20000x20000>" $_.Name ($_.BaseName + ".jp2") 
-				.\ImageMagick\convert.exe $_.Name -resample 96 -resize "100x100>" ($_.BaseName + ".jpg.jpg") 
+				.\ImageMagick\convert.exe -resample 300 -resize "5000x5000>" $_.Name ($_.BaseName + ".jpg") 
+				.\ImageMagick\convert.exe -resize "15000x15000>" $_.Name ($_.BaseName + ".jp2")  
+				.\ImageMagick\convert.exe -thumbnail 100x100 $_.Name ($_.BaseName + ".jpg.jpg")  
 			}
 
 		Else
 			{
-				.\ImageMagick\convert.exe $_.FullName -resample 300 ($_.BaseName + ".jpg") 
+				.\ImageMagick\convert.exe -resample 300 $_.Name ($_.BaseName + ".jpg") 
 				.\ImageMagick\convert.exe $_.Name ($_.BaseName + ".jp2") 
-				.\ImageMagick\convert.exe $_.Name -resample 96 -resize "100x100>" ($_.BaseName + ".jpg.jpg") 
+				.\ImageMagick\convert.exe -thumbnail 100x100 $_.Name ($_.BaseName + ".jpg.jpg") 
 			}
 	}
