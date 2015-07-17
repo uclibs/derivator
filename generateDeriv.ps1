94f5f2da7308f4902b76a08110558bf171d79a30
tallmann <tallmann@ucmail.uc.edu>
2015-07-17 16:44:20 -0400

    Add recursion

--- generateDeriv.ps1
+++ generateDeriv.ps1
@@ -1,31 +1,30 @@
 # UC Libraries, DigitzeUC Spokes
 # Generate derivatives for DRC
 
-# DO I need to add depth 8? autodetect gray vs color depth?
-
-Get-ChildItem "." -filter *.tif | `
+Get-ChildItem "." -recurse -filter *.tif | `
 	Foreach {
 		$image = New-Object -ComObject Wia.ImageFile            
 		$image.LoadFile($_.FullName)            
 
 		If ($image.Width -gt 5000 -lt 15000 -Or $image.Height -gt 5000 -lt 15000)
 			{
-				.\ImageMagick\convert.exe -resample 300 -resize "5000x5000>" $_.Name ($_.BaseName + ".jpg") 
-				.\ImageMagick\convert.exe $_.Name ($_.BaseName + ".jp2") 
-				.\ImageMagick\convert.exe -thumbnail 100x100 $_.Name ($_.BaseName + ".jpg.jpg") 
+				.\ImageMagick\convert.exe -resample 300 -resize "5000x5000>" $_.FullName ($_.DirectoryName + "\" + $_.BaseName + ".jpg") 
+				.\ImageMagick\convert.exe $_.FullName ($_.DirectoryName + "\" + $_.BaseName + ".jp2") 
+				.\ImageMagick\convert.exe -thumbnail 100x100 $_.FullName ($_.DirectoryName + "\" + $_.BaseName + ".jpg.jpg") 
 			}
 
 		ElseIf ($image.Width -ge 15000 -Or $image.Height -ge 15000)
 			{
-				.\ImageMagick\convert.exe -resample 300 -resize "5000x5000>" $_.Name ($_.BaseName + ".jpg") 
-				.\ImageMagick\convert.exe -resize "15000x15000>" $_.Name ($_.BaseName + ".jp2")  
-				.\ImageMagick\convert.exe -thumbnail 100x100 $_.Name ($_.BaseName + ".jpg.jpg")  
+				.\ImageMagick\convert.exe -resample 300 -resize "5000x5000>" $_.FullName ($_.DirectoryName + "\" + $_.BaseName + ".jpg") 
+				.\ImageMagick\convert.exe -resize "15000x15000>" $_.FullName ($_.DirectoryName + "\" + $_.BaseName + ".jp2")  
+				.\ImageMagick\convert.exe -thumbnail 100x100 $_.FullName ($_.DirectoryName + "\" + $_.BaseName + ".jpg.jpg")  
 			}
 
 		Else
 			{
-				.\ImageMagick\convert.exe -resample 300 $_.Name ($_.BaseName + ".jpg") 
-				.\ImageMagick\convert.exe $_.Name ($_.BaseName + ".jp2") 
-				.\ImageMagick\convert.exe -thumbnail 100x100 $_.Name ($_.BaseName + ".jpg.jpg") 
+				.\ImageMagick\convert.exe -resample 300 $_.FullName ($_.DirectoryName + "\" + $_.BaseName + ".jpg") 
+				.\ImageMagick\convert.exe $_.FullName ($_.DirectoryName + "\" + $_.BaseName + ".jp2") 
+				.\ImageMagick\convert.exe -thumbnail 100x100 $_.FullName ($_.DirectoryName + "\" + $_.BaseName + ".jpg.jpg") 
 			}
+        
 	}
